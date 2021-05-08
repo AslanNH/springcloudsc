2ae9d6b9d3c70895b3f07b74c48a1bc26536906d
nihh <nihh@glodon.com>
2021-05-08 18:34:12 +0800

    sleuth+zipkin+mysql

--- sleuth/zipkinserver/src/main/java/com/nh/zipkinserver/ZipkinserverApplication.java
+++ sleuth/zipkinserver/src/main/java/com/nh/zipkinserver/ZipkinserverApplication.java
@@ -1,9 +1,15 @@
 package com.nh.zipkinserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import zipkin.storage.mysql.MySQLStorage;
 import zipkin2.server.internal.EnableZipkinServer;
+import zipkin2.storage.StorageComponent;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +18,11 @@ public class ZipkinserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinserverApplication.class, args);
+    }
+
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource dataSource){
+        return MySQLStorage.builder().datasource(dataSource).executor(Runnable::run).build();
     }
 
 }
